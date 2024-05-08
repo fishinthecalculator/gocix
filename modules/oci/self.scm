3613e35f184c6bc14ebfb69ec15b2d148d902cbb
Giacomo Leidi <goodoldpaul@autistici.org>
2024-05-08 15:52:35 +0200

    oci: self: Add %oci-channel-url.

---
+++ modules/oci/self.scm
@@ -0,0 +1,6 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2024 Giacomo Leidi <goodoldpaul@autistici.org>
+
+(define-module (oci self))
+
+(define-public %oci-channel-url "https://git.sr.ht/~fishinthecalculator/gocix")
