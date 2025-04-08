1690072937daa237f9f96151a1870b77c442b76f
Giacomo Leidi <goodoldpaul@autistici.org>
2025-04-08 18:55:12 +0200

    Revert "services: tandoor: Fix postgresql-role extension point."
    This reverts commit 34199b819cc4da18ccc0e4112767f8eee2777ea2.

--- modules/oci/services/tandoor.scm
+++ modules/oci/services/tandoor.scm
@@ -360,7 +360,6 @@ and returns Tandoor's sh command."
                                            (list
                                             (postgresql-role
                                              (name (tandoor-configuration-postgres-db config))
-                                             (requirement '(sops-secrets))
                                              (password-file
                                               (%tandoor-secrets-postgres-password-file oci-config))
                                              (create-database? #t)))
