47930fe5ebfe05b964ac737ba3a9f9ed9a58f5d7
Giacomo Leidi <goodoldpaul@autistici.org>
2025-05-04 19:57:42 +0200

    services: meilisearch: Generalize secrets handling.

--- modules/oci/services/meilisearch.scm
+++ modules/oci/services/meilisearch.scm
@@ -13,6 +13,7 @@
   #:use-module (guix i18n)
   #:use-module (ice-9 match)
   #:use-module (sops secrets)
+  #:use-module (sops utils)
   #:use-module (sops services sops)
   #:use-module (oci services configuration)
   #:use-module (oci services containers)
@@ -23,6 +24,7 @@
             oci-meilisearch-configuration-port
             oci-meilisearch-configuration-datadir
             oci-meilisearch-configuration-database-path
+            oci-meilisearch-configuration-secrets-directory
             oci-meilisearch-configuration-network
             oci-meilisearch-configuration-extra-variables
             oci-meilisearch-configuration->oci-container-configuration
@@ -64,6 +66,9 @@ This means you will need a valid API key to access all other endpoints.")
   (database-path
    (string "/var/lib/meilisearch/data.ms")
    "The directory used by meilisearch database to store state.")
+  (secrets-directory
+   (string "/run/secrets")
+   "The directory where secrets are looked for.")
   (network
    (maybe-string)
    "The docker network where the meilisearch container will be attached. When equal
@@ -71,6 +76,20 @@ to \"host\" the @code{port} field will not be mapped into the container's one.")
   (extra-variables
    (list '())
    "A list of pairs representing any extra environment variable that should be set inside the container. Refer to the @uref{upstream, https://www.meilisearch.com/docs/learn/configuration/instance_options} documentation for more details."))
+
+(define (%meilisearch-secrets config)
+  (list (oci-meilisearch-configuration-master-key config)))
+
+(define %meilisearch-secrets-variables
+  '("MEILI_MASTER_KEY"))
+
+(define (%meilisearch-secret-file config secret)
+  (string-append (oci-meilisearch-configuration-secrets-directory config)
+                 "/" (sops-secret->file-name secret)))
+
+(define (%meilisearch-secrets-files config)
+  (map (lambda (s) (%meilisearch-secret-file config s))
+       (%meilisearch-secrets config)))
 
 (define (%meilisearch-activation config)
   "Return an activation gexp for Meilisearch."
@@ -99,13 +118,21 @@ to \"host\" the @code{port} field will not be mapped into the container's one.")
             (oci-meilisearch-configuration-image config))
            (port
             (oci-meilisearch-configuration-port config))
+           (secrets
+            (delete-duplicates
+             (map (lambda (secret-file)
+                    (string-append secret-file ":" secret-file ":ro"))
+                  (%meilisearch-secrets-files config))))
            (container-config
             (mainline:oci-container-configuration
              (image image)
              (requirement '(sops-secrets))
              (entrypoint "/sbin/tini")
              (command
-              `("--" "sh" "-c" "export MEILI_MASTER_KEY=$(cat /run/secrets/meilisearch/master) && exec /bin/meilisearch"))
+              `("--" "sh" "-c"
+                ,(sops-secrets-sh-command-wrapper
+                  (%tandoor-secrets-specs config)
+                  '("/bin/meilisearch"))))
              (environment
               (append
                '(("MEILI_NO_ANALYTICS" . "true"))
@@ -115,7 +142,7 @@ to \"host\" the @code{port} field will not be mapped into the container's one.")
              (volumes
               `((,datadir . "/meili_data")
                 (,database-path . "/data.ms")
-                ("/run/secrets/meilisearch" . "/run/secrets/meilisearch:ro"))))))
+                ,@secrets)))))
       (list
        (if (maybe-value-set? network)
            (mainline:oci-container-configuration
