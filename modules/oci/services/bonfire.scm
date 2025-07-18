a8d169580570e5917f70cb41c920bcac18c8aa8a
Giacomo Leidi <goodoldpaul@autistici.org>
2025-07-18 23:15:33 +0200

    services: bonfire: Update to 1.0.0-rc.2.1.

--- modules/oci/services/bonfire.scm
+++ modules/oci/services/bonfire.scm
@@ -73,7 +73,7 @@
             oci-bonfire-service-type))
 
 (define %bonfire-version
-  "1.0.0-rc.1.24")
+  "1.0.0-rc.2.1")
 
 (define (bonfire-tag flavour system)
   (string-join (list %bonfire-version flavour system) "-"))
