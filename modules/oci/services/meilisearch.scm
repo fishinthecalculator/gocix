3bed9792a43699e72b3642ee6b884a04bcb46df5
Giacomo Leidi <goodoldpaul@autistici.org>
2025-10-07 00:11:48 +0200

    services: meilisearch: Drop hard requirement on sops-guix.
    * modules/oci/services/meilisearch.scm
    (oci-meilisearch-configuration)[secrets-directory]: Drop field.
    [shepherd-requirement]: New field.
    [master-key]: Change field type to string.
    (oci-meilisearch-configuration->oci-container-configuration): Use
    shepherd-requirement field and string type secret field.
    (oci-meilisearch-service-type): Drop sops-secrets-service-type extension point.

--- modules/oci/services/meilisearch.scm
+++ modules/oci/services/meilisearch.scm
@@ -10,9 +10,7 @@
   #:use-module (gnu system shadow)
   #:use-module (guix diagnostics)
   #:use-module (guix gexp)
-  #:use-module (sops secrets)
   #:use-module (sops utils)
-  #:use-module (sops services sops)
   #:use-module (oci build utils)
   #:use-module (oci services configuration)
   #:use-module (srfi srfi-1)
@@ -23,9 +21,9 @@
             oci-meilisearch-configuration-fields
             oci-meilisearch-configuration-image
             oci-meilisearch-configuration-port
+            oci-meilisearch-configuration-shepherd-requirement
             oci-meilisearch-configuration-datadir
             oci-meilisearch-configuration-database-path
-            oci-meilisearch-configuration-secrets-directory
             oci-meilisearch-configuration-network
             oci-meilisearch-configuration-extra-variables
             oci-meilisearch-configuration->oci-container-configuration
@@ -57,26 +55,32 @@
   (port
    (string "7700")
    "The port where meilisearch will be exposed.")
+  (shepherd-requirement
+   (list '(user-processes))
+   "A list of Shepherd services that will be waited for before starting
+Meilisearch.")
   (master-key
-   (sops-secret)
-   "Sets the instance's master key, automatically protecting all routes except GET /health.
-This means you will need a valid API key to access all other endpoints.")
+   (string)
+   "The file name of the secrets containing the instance's master key,
+automatically protecting all routes except GET /health.  This means you will
+need a valid API key to access all other endpoints.")
   (datadir
    (string "/var/lib/meilisearch/meili_data")
    "The directory where meilisearch writes state.")
   (database-path
    (string "/var/lib/meilisearch/data.ms")
    "The directory used by meilisearch database to store state.")
-  (secrets-directory
-   (string "/run/secrets")
-   "The directory where secrets are looked for.")
   (network
    (maybe-string)
-   "The docker network where the meilisearch container will be attached. When equal
-to \"host\" the @code{port} field will not be mapped into the container's one.")
+   "The docker network where the meilisearch container will be attached. When
+equal to \"host\" the @code{port} field will not be mapped into the container's
+one.")
   (extra-variables
    (list '())
-   "A list of pairs representing any extra environment variable that should be set inside the container. Refer to the @uref{upstream, https://www.meilisearch.com/docs/learn/configuration/instance_options} documentation for more details."))
+   "A list of pairs representing any extra environment variable that should be
+set inside the container.  Refer to the
+@uref{upstream,https://www.meilisearch.com/docs/learn/configuration/instance_options}
+documentation for more details."))
 
 (define (%meilisearch-secrets config)
   (list (oci-meilisearch-configuration-master-key config)))
@@ -84,15 +88,8 @@ to \"host\" the @code{port} field will not be mapped into the container's one.")
 (define %meilisearch-secrets-variables
   '("MEILI_MASTER_KEY"))
 
-(define (%meilisearch-secret-file config secret)
-  (string-append (oci-meilisearch-configuration-secrets-directory config)
-                 "/" (sops-secret->file-name secret)))
-
-(define (%meilisearch-secrets-files config)
-  (map (lambda (s) (%meilisearch-secret-file config s))
-       (%meilisearch-secrets config)))
-
-(define (%meilisearch-secrets-specs config) (zip %meilisearch-secrets-variables (%meilisearch-secrets-files config)))
+(define (%meilisearch-secrets-specs config)
+  (zip %meilisearch-secrets-variables (%meilisearch-secrets config)))
 
 (define (%meilisearch-activation config)
   "Return an activation gexp for Meilisearch."
@@ -126,13 +123,15 @@ to \"host\" the @code{port} field will not be mapped into the container's one.")
             (oci-meilisearch-configuration-image config))
            (port
             (oci-meilisearch-configuration-port config))
+           (requirement
+            (oci-meilisearch-configuration-shepherd-requirement config))
            (secrets
             (secrets-volume-mappings
-             (%meilisearch-secrets-files config)))
+             (%meilisearch-secrets config)))
            (container-config
             (oci-container-configuration
              (image image)
-             (requirement '(sops-secrets))
+             (requirement requirement)
              (entrypoint "/sbin/tini")
              (command
               `("--" "sh" "-c"
@@ -164,9 +163,6 @@ to \"host\" the @code{port} field will not be mapped into the container's one.")
                                                        (oci-extension
                                                         (containers
                                                          (oci-meilisearch-configuration->oci-container-configuration config)))))
-                                  (service-extension sops-secrets-service-type
-                                                     (lambda (config)
-                                                       (list (oci-meilisearch-configuration-master-key config))))
                                   (service-extension activation-service-type
                                                      %meilisearch-activation)))
                 (description
